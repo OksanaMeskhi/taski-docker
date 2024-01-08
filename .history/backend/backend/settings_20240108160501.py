998a97008c289a38cfbd9a1bb7dd863d58ed7e03
OksanaMeskhi <oks.meskhi@gmail.com>
2024-01-08 16:06:32 +0300

    deploy

--- .history/backend/backend/settings_20240108160501.py
+++ .history/backend/backend/settings_20240108160501.py
@@ -1,13 +1,13 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = 'SECRET_KEY '
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.80.149', '127.0.0.1', 'localhost', 'taski17.ddns.net']
 
 
 # Application definition
@@ -88,7 +88,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
